046c6237324105875c49936b9a27582db863753c
Khitoshi <kubotah1115@gmail.com>
2024-12-24 13:27:24 +0000

    WriteResult関数を追加し、言語ごとのファイル解析結果を整形して出力する機能を実装

--- src/main.go
+++ src/main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	clocexplorer "github.com/Khitoshi/cloc-explorer"
 )
 
+func WriteResult(languages clocexplorer.DefinedLanguages) {
+	// write header
+	//o.WriteHeader()
+	const commonHeader string = "name                           files          blank        comment           code"
+	const defaultOutputSeparator string = "-------------------------------------------------------------------------" +
+		"-------------------------------------------------------------------------"
+	//"-------------------------------------------------------------------------"
+	fmt.Println(commonHeader)
+	fmt.Println(defaultOutputSeparator)
+
+	totalFiles := uint64(0)
+	totalBlanks := uint64(0)
+	totalComments := uint64(0)
+	totalCode := uint64(0)
+
+	for _, lang := range languages.Langs {
+		fmt.Printf("%-27v %6v %14v %14v %14v\n", lang.Name, len(lang.Files), lang.Blanks, lang.Comments, lang.Code)
+
+		totalFiles += uint64(len(lang.Files))
+		totalBlanks += lang.Blanks
+		totalComments += lang.Comments
+		totalCode += lang.Code
+	}
+
+	// write footer
+	//o.WriteFooter()
+	//fmt.Println('\n')
+	fmt.Printf("\n")
+	fmt.Printf("%-27v %6v %14v %14v %14v\n", "Total", totalFiles, totalBlanks, totalComments, totalCode)
+}
+
 func main() {
-	ri, err := clocexplorer.NewRepositoryInfo("Khitoshi/GameGraphicsLibrary", "main")
+	ri, err := clocexplorer.NewRepositoryInfo("Khitoshi/gocloc", "master")
 	if err != nil {
 		log.Println(err)
 		return
@@ -24,11 +56,12 @@ func main() {
 	language := clocexplorer.NewFileData(paths, *languages)
 	language = clocexplorer.AnalyzeFile(language, ri)
 
-	for lang, langData := range language.Langs {
-		log.Printf("言語:%s", lang)
-		log.Printf("Comments:%d", langData.Comments)
-		log.Printf("Code:%d", langData.Code)
-		log.Printf("Blanks:%d", langData.Blanks)
-		log.Printf("\n\n")
-	}
+	//for lang, langData := range language.Langs {
+	//	log.Printf("言語:%s", lang)
+	//	log.Printf("Comments:%d", langData.Comments)
+	//	log.Printf("Code:%d", langData.Code)
+	//	log.Printf("Blanks:%d", langData.Blanks)
+	//	log.Printf("\n\n")
+	//}
+	WriteResult(language)
 }
